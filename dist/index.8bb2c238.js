584cb040465afe2ba6443fcdbe4b56fe245272f3
mulaz9 <davide.mulato@gmail.com>
2022-11-25 16:07:26 +0100

    first commit

---
+++ dist/index.8bb2c238.js
@@ -0,0 +1,34 @@
+"use strict";
+const body = document.getElementById("body");
+const generateMarkup = function() {
+    return /*html*/ `
+  <nav class="navbar">
+  <div class="navbar__logo">
+    <img src="https://res.cloudinary.com/mulaz/image/upload/v1669155224/logo_light_htlfqo.jpg" alt="logo" />
+  </div>
+  <ul class="navbar__ul">
+    <li>Home</li>
+    <li>Chi<span>_</span>Siamo</li>
+    <li>Certificazioni</li>
+    <li>Prodotti</li>
+    <li>Realizzazioni</li>
+    <li>News</li>
+    <li>Contatti</li>
+  </ul>
+</nav>
+    `;
+};
+const markup = generateMarkup();
+body.insertAdjacentHTML("afterbegin", markup);
+// Navbar a scomparsa
+const nav = document.querySelector(".navbar");
+let lastScrollY = window.scrollY;
+window.addEventListener("scroll", function() {
+    if (this.window.scrollY > 1400) {
+        nav.style.transform = "translateY(-100%)";
+        if (lastScrollY > this.window.scrollY) nav.style.transform = "translateY(0)";
+    } else nav.style.transform = "translateY(0)";
+    lastScrollY = window.scrollY;
+}); // Footer
+
+//# sourceMappingURL=index.8bb2c238.js.map
---
+++ js/slider_home.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const splide = new Splide(".splide", {
+  type: "loop",
+  perPage: 1,
+  arrows: false,
+  pagination: false,
+  autoplay: true,
+  speed: 1500,
+  pauseOnHover: false,
+  updateOnMove: true,
+  keyboard: "global",
+});
+
+const splideTrack = document.querySelector(".splide__track");
+const bar = document.querySelector(".my-slider-progress-bar");
+const slide1 = document.querySelector(".h1__hero1");
+const slide2 = document.querySelector(".h1__hero2");
+const slide3 = document.querySelector(".h1__hero3");
+const btnSlide1 = document.querySelector(".btn__slide--1");
+const btnSlide2 = document.querySelector(".btn__slide--2");
+const btnSlide3 = document.querySelector(".btn__slide--3");
+
+// Updates the bar width whenever the carousel moves:
+splide.on("mounted move", function () {
+  const end = splide.Components.Controller.getEnd() + 1;
+  const rate = Math.min((splide.index + 1) / end, 1);
+  bar.style.width = String(100 * rate) + "%";
+  splideTrack.addEventListener("mousedown", function () {
+    splideTrack.style.cursor = "grabbing";
+  });
+  splideTrack.addEventListener("mouseup", function () {
+    splideTrack.style.cursor = "grab";
+  });
+  if (rate < 0.4) {
+    slide1.style.transform = "translateY(0)";
+    btnSlide1.style.transform = "translateY(0)";
+    slide2.style.transform = "translateY(30px)";
+    btnSlide2.style.transform = "translateY(60px)";
+    slide3.style.transform = "translateY(30px)";
+    btnSlide3.style.transform = "translateY(60px)";
+  }
+  if (rate > 0.4 && rate < 1) {
+    slide1.style.transform = "translateY(30px)";
+    btnSlide1.style.transform = "translateY(60px)";
+    slide2.style.transform = "translateY(0)";
+    btnSlide2.style.transform = "translateY(0)";
+    slide3.style.transform = "translateY(30px)";
+    btnSlide3.style.transform = "translateY(60px)";
+  } else {
+    slide2.style.transform = "translateY(30px)";
+    btnSlide2.style.transform = "translateY(60px)";
+    slide3.style.transform = "translateY(0)";
+    btnSlide3.style.transform = "translateY(0)";
+  }
+});
+splide.mount();
