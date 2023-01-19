70f233fca0ae317cc3cc7fdca7e6db251f302d11
mulaz9 <davide.mulato@gmail.com>
2023-01-19 10:27:10 +0100

    iso-logo

--- dist/index.de343c3b.js
+++ dist/index.de343c3b.js
@@ -210,7 +210,7 @@ const footerMarkup = function() {
       width="60"
     ></iconify-icon
   ></a>
-  <a href="#" class="iubenda-cs-preferences-link">Aggiorna le tue preferenze di tracciamento della pubblicità</a>
+  <a href="#" class="iubenda-cs-preferences-link">Aggiorna le tue preferenze di <br> tracciamento della pubblicità</a>
 </div>
 <h6 class="copyright">
 <a href="https://www.iubenda.com/privacy-policy/41291582" class="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe " title="Privacy Policy ">Privacy Policy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script><br>
